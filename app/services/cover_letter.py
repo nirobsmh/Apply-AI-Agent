de270ea7be23b2ac8c2533ce7875c6468b357be3
nirobsmh <smh.nirob@gmail.com>
2026-03-22 23:28:27 +0600

    cover letter

---
+++ app/services/cover_letter.py
@@ -0,0 +1,14 @@
+from app.schemas.response import CoverLetterResponse
+from app.services.llm_service import load_prompt
+from app.services.llm_service import call_llm
+
+def cover_letter_f(resume_text: str, job_description: str) -> CoverLetterResponse:
+    """Write a cover letter based on the resume and job description."""
+    try:
+        prompt = load_prompt("app/prompts/cover_letter.txt")
+        inputText = "Resume text: " + resume_text + "\n\n" + "Job description: " + job_description
+        response = call_llm(prompt, inputText)
+        return CoverLetterResponse.model_validate(response)
+    except Exception as e:
+        print("Error in cover_letter_f: ", e)
+        return CoverLetterResponse(cover_letter="")
